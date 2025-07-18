687bc8207c3ecc4c474bca88e0a743f02c5f128e
taeseonYoo <jkl0642@hansung.ac.kr>
2025-07-18 13:36:56 +0900

    fix: 챗 Api

--- src/main/kotlin/com/tae/backend/chat/ui/ChatController.kt
+++ src/main/kotlin/com/tae/backend/chat/ui/ChatController.kt
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/chat")
+@RequestMapping("/api/chats")
 class ChatController (
         private val chatService : ChatService
 ){
